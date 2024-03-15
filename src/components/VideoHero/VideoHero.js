79507411e334f403642dd8ac3bfd0ceda195dc2a
SolomonPipi <solomonpipi1@gmail.com>
2024-03-15 16:38:19 +0000

    changed the width of the sidebar

--- src/components/VideoHero/VideoHero.js
+++ src/components/VideoHero/VideoHero.js
@@ -3,13 +3,17 @@ import "./VideoHero.scss";
 export default function VideoHero({ activeVideo }) {
   return (
     <>
-      {activeVideo && (
-        <video
-          className="VideoHero"
-          poster={activeVideo.image}
-          controls
-        ></video>
-      )}
+      <section className="VideoHero">
+        <div className="VideoHero__container">
+          {activeVideo && (
+            <video
+              className="VideoHero__img"
+              poster={activeVideo.image}
+              controls
+            ></video>
+          )}
+        </div>
+      </section>
     </>
   );
 }
