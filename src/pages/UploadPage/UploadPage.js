87e517694d079c0043f19a44d198f7b8b05d167c
SolomonPipi <solomonpipi1@gmail.com>
2024-03-20 16:41:22 +0000

    added styling divs

--- src/pages/UploadPage/UploadPage.js
+++ src/pages/UploadPage/UploadPage.js
@@ -9,7 +9,7 @@ export default function UploadPage() {
 
   const nav = useNavigate();
 
-  const publishNav = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
     axios.post("http://localhost:5051/videos/", {
@@ -30,34 +30,38 @@ export default function UploadPage() {
         <div className="UploadPage__container">
           <h1 className="UploadPage__title">Upload Video</h1>
           <div className="UploadPage__body">
-            <div className="UploadPage__thumbnail">
-              <p className="UploadPage__subtitle">video thumbnail</p>
-              <img
-                src={uploadImage}
-                alt="Track Runner"
-                className="UploadPage__img"
-              />
-            </div>
-            <form onSubmit={publishNav} className="UploadPage__form">
-              <label htmlFor="video__title" className="UploadPage__label">
-                title your video
-              </label>
-              <input
-                id="title"
-                name="video__title"
-                type="text"
-                className="UploadPage__input"
-                placeholder="Add a title to your video"
-              />
-              <label htmlFor="video__desc" className="UploadPage__label">
-                add a video description
-              </label>
-              <textarea
-                id="description"
-                name="video__desc"
-                className="UploadPage__input--description"
-                placeholder="Add a description to your video"
-              ></textarea>
+            <form onSubmit={handleSubmit} className="UploadPage__form">
+              <div className="uploadPage__containers">
+                <div className="UploadPage__thumbnail">
+                  <p className="UploadPage__subtitle">video thumbnail</p>
+                  <img
+                    src={uploadImage}
+                    alt="Track Runner"
+                    className="UploadPage__img"
+                  />
+                </div>
+                <div className="uploadPage__form-container">
+                  <label htmlFor="video__title" className="UploadPage__label">
+                    title your video
+                  </label>
+                  <input
+                    id="title"
+                    name="video__title"
+                    type="text"
+                    className="UploadPage__input"
+                    placeholder="Add a title to your video"
+                  />
+                  <label htmlFor="video__desc" className="UploadPage__label">
+                    add a video description
+                  </label>
+                  <textarea
+                    id="description"
+                    name="video__desc"
+                    className="UploadPage__input--description"
+                    placeholder="Add a description to your video"
+                  ></textarea>
+                </div>
+              </div>
               <div className="UploadPage__buttons">
                 <button className="UploadPage__publish">Publish</button>
                 <Link to="/">
@@ -66,6 +70,7 @@ export default function UploadPage() {
               </div>
             </form>
           </div>
+
           {message && <p>Published</p>}
         </div>
       </article>
